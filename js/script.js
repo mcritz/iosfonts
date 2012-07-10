2492cf05bd7f368e0426a56c895295f37cd4fa3e
Michael Critz <github@micahelcritz.com>
2012-07-10 08:18:43 -0700

    Added texture, search, filtering, targeting, more styles
    Need to increase number of local commits to prevent major, massive,
    fuck-ups

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 var previousSelectedOS ='';
 var selectedOS = '';
+var searchActive = false;
+
+// Extend :contains() method to :containsi, a case-insensitive version of :contains()
+$.extend($.expr[':'], {
+  'containsi': function(elem, i, match, array)
+  {
+    return (elem.textContent || elem.innerText || '').toLowerCase()
+    .indexOf((match[3] || "").toLowerCase()) >= 0;
+  }
+});
+
+
 $(document).ready(function(){
+$('header#main').append('<div class="count"><div class="font_families">Families: <b></b></div><div class="font_faces">Faces: <b></b></div></div>');
+countFonts();
 /* 	$('hgroup').append('<p>jQuery is loaded.</p>'); */
 
 /* $('nav .ios50').toggleClass('selected'); */
@@ -11,6 +25,7 @@ $('nav li').click(function(){
   if (selectedOS == previousSelectedOS){
  	 $('tr').removeClass('unavailable');
  	 previousSelectedOS = '';
+ 	 countFonts();
  	 return;
   } else {
 	  $(this).toggleClass('selected');
@@ -35,13 +50,53 @@ $('nav li').click(function(){
 	      alert('Oops. There was an error.');
 	  }
 	  previousSelectedOS = selectedOS;
+	  countFonts();
   }
 });
 
-
+// search
+$('input[type="search"]').keyup(function(){
+	theSearch = $('input[type="search"]').val();
+	// console.log('theSearch: ' + theSearch);
+	searchPage(theSearch);
+	searchActive = true;
+	countFonts();
+});
+// clear search
+$('input[type="search"]').on("search", function(e){
+	//alert('hi')}
+	clearSearch();
+});
 }); // end drf
 
+function searchPage(searchTerm){
+  if(searchTerm.length == 0){
+    clearSearch();
+   //  $('.btn_clear_search').fadeOut(200);
+  } else if(searchTerm.length > 0){
+    $('tr:containsi("' + searchTerm + '")').fadeIn(150);
+    $('tr:not(:containsi("' + searchTerm + '"))').fadeOut(250);
+    $('tr').find(searchTerm).css('color','red');
+  } else {
+    clearSearch();
+  }
+}
 
+function clearSearch() {
+  $('input[type="search"]').val('');
+  $('tr').fadeIn(250);
+  searchActive = false;
+  countFonts();
+}
+
+// count fonts
+function countFonts() {
+	numberOfFontFamilies = $('td.rowheader').filter(':visible').length;
+	numberOfFontFaces = $('tr').not('.unavailable').has('td').not('.rowheader').filter(':visible').length; // .has(!$('td.rowheader')).filter(':visible').
+	console.log(' numberOfFontFamilies: ' + numberOfFontFamilies + '\n numberOfFontFaces: ' + numberOfFontFaces);
+	$('.count .font_families b').html(numberOfFontFamilies);
+	$('.count .font_faces b').html(numberOfFontFaces);
+}
 
 // ga
 var _gaq = _gaq || [];
